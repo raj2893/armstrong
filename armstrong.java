0feef4afabdb5fc98eef218971c3175356341b3d
Raj <praj11609@gmail.com>
2022-01-20 22:09:54 +0530

    Final commit for armstrong code using java...

---
+++ armstrong.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class armstrong {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        // a= 153 --> 1 cube + 5 cube + 3 cube = same number
+        System.out.println(isArmstrong(n));
+        for (int i = 100; i < 1000; i++){
+            if (isArmstrong(i)){
+                System.out.println(i + " ");
+            }
+        }
+    }
+    static boolean isArmstrong(int n){
+        int original = n;
+        int sum = 0;
+        while (n > 0) {
+            int rem = n % 10;
+            n = n/10;
+            sum = sum + rem*rem*rem;
+        }
+        return sum == original;
+    }
+}
