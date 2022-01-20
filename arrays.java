63983549f0e298267170449aaa24ab8e74f0aa79
Raj <praj11609@gmail.com>
2022-01-21 00:27:01 +0530

    Final code is ready...

---
+++ arrays.java
@@ -0,0 +1,11 @@
+public class arrays {
+    public static void main(String[] args) {
+        // store 5 roll numbers
+        int[] rnos = new int[5];
+        // or
+        int[] rnos2 = {23, 12, 51, 46, 45};
+        String[] arr = new String[4];
+        System.out.println(arr[2]);
+
+    }
+}
