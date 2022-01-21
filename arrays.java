0b669be20a88cd3fd1f9e2d0d94778520cac0ce2
Raj <praj11609@gmail.com>
2022-01-21 11:08:53 +0530

    done...

--- arrays.java
+++ arrays.java
@@ -7,13 +7,13 @@ public class arrays {
         int[] demo = new int[5];
         String[] str = new String[5];
 
-//        for (int i = 0; i < demo.length; i++){
-//            System.out.println("Input the " + i + " the member of array");
-//            demo[i] = in.nextInt();
-//        }
-//        for (int i = 0; i < demo.length; i++){
-//            System.out.println(demo[i]);
-//        }
+        for (int i = 0; i < demo.length; i++){
+            System.out.println("Input the " + i + " the member of array");
+            demo[i] = in.nextInt();
+        }
+        for (int i = 0; i < demo.length; i++){
+            System.out.println(demo[i]);
+        }
         for (int j = 0; j < str.length; j++) {
             System.out.println("Input the " + j + "th member of array");
             str[j] = in.next();
